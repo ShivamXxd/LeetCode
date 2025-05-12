ab384923917684742b5656ae02ca26d2a3c80bc4
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-05-12 14:13:02 +0530

    Time: 0 ms (100%), Space: 18 MB (8.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Initialize memoization array
         memo = [0] * (n + 1)
-
-        # Base cases
         if n == 1:
             return 1
         elif n == 2:
             return 2
-
-        # Set base cases in the memo array
+        elif n == 3:
+            return 3
         memo[1] = 1
         memo[2] = 2
-
-        # Calculate the number of ways to climb stairs iteratively
-        for i in range(3, n + 1):
+        memo[3] = 3
+        for i in range(4, n + 1):
             memo[i] = memo[i - 1] + memo[i - 2]
 
         return memo[n]
-
-# Test the function with an example
-sol = Solution()
-sol.climbStairs(5)  # Example input
