13ed87b594b21967d4748e62a159e7bf421452cb
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-07 10:31:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (54.29%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -7,13 +7,15 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         def isMirror(left, right):
-            if not left and not right:
+            if not left and not right: # if both are None, it means theyre equal
                 return True
-            if not left or not right:
+            if not left or not right: # if one of them is None, then its not mirror
                 return False
-            if left.val != right.val:
+            if left.val != right.val: # compare for equal values
                 return False
             return isMirror(left.left, right.right) and isMirror(left.right, right.left)
-        if not root:
+            # left subtree's left node should be equal to right subtree's right node for symmetry.
+            # AND left subtree's right node should be equal to right subtree's left node for symmetry.
+        if not root: # if the tree is empty, it is mirror of itself.
             return True
         return isMirror(root.left, root.right)
