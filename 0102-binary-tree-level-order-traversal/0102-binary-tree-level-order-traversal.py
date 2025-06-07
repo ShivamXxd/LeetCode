2f22532a6d98c257d4c22ba745070b75f87f3a7e
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-07 13:15:41 +0530

    Time: 0 ms (100%), Space: 18.5 MB (59.74%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -9,17 +9,18 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        dq = deque([root])
-        result = []
+        dq = deque([root]) # store the root node in it
+        result = [] # contains all levels
         while dq:
-            level = []
+            level = [] # every level's nodes will be saved
             for _ in range(len(dq)):
-                node = dq.popleft()
-                level.append(node.val)
+                node = dq.popleft() # pop from left cuz we want nodes from left to right
+                level.append(node.val) # add in current level
 
-                if node.left:
-                    dq.append(node.left)
-                if node.right:
+                if node.left: # if there is left child then add it in queue
+                    dq.append(node.left) # we popped the root but left and right child nodes will be in queue
+                if node.right: 
                     dq.append(node.right)
+                # we added child nodes in queue and traverse through it popping them and adding in current level till queue is empty
             result.append(level)
         return result    
