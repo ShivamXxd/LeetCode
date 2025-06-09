c6fc2cb35ab4efbe1c7364c8fd1cf25e228575fb
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-09 08:59:00 +0530

    Time: 0 ms (100%), Space: 50.6 MB (9.37%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -6,14 +6,30 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+        # def minDepth(root):
+        #     if not root: # depth is 0 when root is absent
+        #         return 0
+        #     if not root.left and not root.right: 
+        #         return 1 # depth is one if tree only has the root
+        #     if not root.left: # if there's no left subtree, go to the right subtree and find shortest leaf node path
+        #         return 1 + minDepth(root.right) # we add 1 cuz everytime we go deeper, depth increases
+        #     if not root.right: # if there's no right subtree, do the same as prior with left subtree
+        #         return 1 + minDepth(root.left)
+        #     return 1 + min(minDepth(root.left), minDepth(root.right)) # find min of left and right subtree
+        # return minDepth(root)
+
+        # BFS Method
+        from collections import deque
         def minDepth(root):
-            if not root: # depth is 0 when root is absent
+            if not root:
                 return 0
-            if not root.left and not root.right: 
-                return 1 # depth is one if tree only has the root
-            if not root.left: # if there's no left subtree, go to the right subtree and find shortest leaf node path
-                return 1 + minDepth(root.right) # we add 1 cuz everytime we go deeper, depth increases
-            if not root.right: # if there's no right subtree, do the same as prior with left subtree
-                return 1 + minDepth(root.left)
-            return 1 + min(minDepth(root.left), minDepth(root.right)) # find min of left and right subtree
+            queue = deque([(root, 1)])  # (node, current_depth)
+            while queue:
+                node, depth = queue.popleft()
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    queue.append((node.left, depth + 1))
+                if node.right:
+                    queue.append((node.right, depth + 1))
         return minDepth(root)
