b1f72d2259d322caa153fbe6d9190bfc29697a8a
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-09 08:26:52 +0530

    Time: 0 ms (100%), Space: 19 MB (78.75%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,12 +1,19 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        max_depth = 0
-        def inorder(node, count):
-            nonlocal max_depth 
-            if not node:
-                max_depth = max(max_depth, count)
-                return
-            inorder(node.left, count + 1) # count increases as the depth increases
-            inorder(node.right, count + 1)
-        inorder(root, 0)
-        return max_depth
+        # max_depth = 0
+        # def inorder(node, count):
+        #     nonlocal max_depth 
+        #     if not node:
+        #         max_depth = max(max_depth, count)
+        #         return
+        #     inorder(node.left, count + 1) # count increases as the depth increases
+        #     inorder(node.right, count + 1)
+        # inorder(root, 0)
+        # return max_depth
+
+        # This postorder code is very commonly used for finding max depth 
+        def maxDepth(root):
+            if not root:
+                return 0
+            return 1 + max(maxDepth(root.left), maxDepth(root.right))
+        return maxDepth(root)
