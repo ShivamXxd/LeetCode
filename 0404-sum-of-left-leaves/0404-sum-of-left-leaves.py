f17f2c9935ddfe3cb9484feef8f6bb757d5efac2
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-09 12:46:32 +0530

    Time: 0 ms (100%), Space: 17.9 MB (78.07%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            total = 0
+            if node.left:
+                if not node.left.left and not node.left.right: # check for leaf node
+                    total += node.left.val
+                else:
+                    total += dfs(node.left) # check for such left leaves in node.left and add all into total
+            total += dfs(node.right) # check same way for right side
+            return total
+        return dfs(root)
