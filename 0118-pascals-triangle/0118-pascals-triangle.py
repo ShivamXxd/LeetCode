c19bdc1980f5725febeeedf4deb2a6eb698df938
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-06-12 10:39:20 +0530

    Time: 0 ms (100%), Space: 17.8 MB (58.5%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,9 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
-        for i in range(numRows):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            triangle.append(row)
+        for i in range(numRows):  # till all rows are done
+            row = [1] * (i + 1)   # create dummy row
+            # the below loop doesn't run for i = 0
+            for j in range(1, i): # start and end will always be 1 so we change the middle elements only
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j] # update the row with last row's j-1 and j being added to make new j
+            triangle.append(row) # append the generated row
         return triangle
