89e0ab941ff0ce6bfd74151152a3c0593c03eabd
ShivamBharti <61899427+ShivamXxd@users.noreply.github.com>
2025-08-26 12:14:57 +0530

    Time: 20 ms (27.74%), Space: 19.9 MB (69.43%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,21 +1,5 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        # candies = 0
-        # if ratings[0] > ratings[1]:
-        #     candies += 2
-        # else: candies += 1
-        # if ratings[len(ratings) - 1] > ratings[len(ratings) - 2]:
-        #     candies += 2
-        # else: candies += 1
-        # for i in range(len(ratings)):
-        #     if i >= 1 and i < len(ratings) - 1:
-        #         if ratings[i] > ratings[i - 1] or ratings[i] > ratings[i+1]:
-        #             candies += 2
-        #         # elif ratings[i] > ratings[i+1]:
-        #         #     candies += 2
-        #         else: 
-        #             candies += 1
-        # return candies
         n = len(ratings)
         candies = [1] * n
 
